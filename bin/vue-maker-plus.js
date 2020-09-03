1311f10345fa1861a4e371ac4055314e761cc6b4
wangchong <wangchong@didachuxing.com>
2020-09-04 00:10:22 +0800

    init2

--- bin/vue-maker-plus.js
+++ bin/vue-maker-plus.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk')
-const program = require('commander')
+const program = require('commander');
 const vueMakerPlus = require('../lib/vue-maker-plus');
 
 program
   .command('create <app-name>')
-  .description('create a new project powered by vue-cli-service')
+  .description('create a new project')
+  .option('-g, --git [message]', 'Force git initialization with initial commit message')
+  .option('-n, --no-git', 'Skip git initialization')
+  .option('-f, --force', 'Overwrite target directory if it exists')
+  .option('--merge', 'Merge target directory if it exists')
   .action((name, cmd) => {
-    require('../lib/create/index')(name)
-  })
+    const options = cleanArgs(cmd);
+    // --git makes commander to default git to true
+    if (process.argv.includes('-g') || process.argv.includes('--git')) {
+      options.forceGit = true
+    }
+    require('../lib/create/index')(name, options);
+  });
 
 program
   .command('serve [entry]')
@@ -18,7 +26,7 @@ program
   .option('-c, --copy', 'Copy local url to clipboard')
   .option('-p, --port <port>', 'Port used by the server (default: 8080 or next available port)')
   .action((entry, cmd) => {
-    vueMakerPlus(cleanArgs(cmd))
+    vueMakerPlus(entry, cleanArgs(cmd));
   });
 
 program
@@ -28,25 +36,38 @@ program
   .option('-n, --name <name>', 'name for lib or web-component mode (default: entry filename)')
   .option('-d, --dest <dir>', 'output directory (default: dist)')
   .action((entry, cmd) => {
-    vueMakerPlus(cleanArgs(cmd))
+    vueMakerPlus(entry, cleanArgs(cmd));
+  });
+
+program
+  .command('inspect [paths...]')
+  .description('inspect the webpack config in a project')
+  .option('--mode <mode>')
+  .option('--rule <ruleName>', 'inspect a specific module rule')
+  .option('--plugin <pluginName>', 'inspect a specific plugin')
+  .option('--rules', 'list all module rule names')
+  .option('--plugins', 'list all plugin names')
+  .option('-v --verbose', 'Show full function definitions in output')
+  .action((entry, cmd) => {
+    vueMakerPlus(entry, cleanArgs(cmd));
   });
 
 program
   .arguments('<command>')
-  .action((entry, cmd) => {
-    vueMakerPlus(cleanArgs(cmd))
+  .action(() => {
+    vueMakerPlus();
   });
 
-program.parse(process.argv)
+program.parse(process.argv);
 
-function camelize (str) {
+function camelize(str) {
   return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
 }
 
 // commander passes the Command object itself as options,
 // extract only actual options into a fresh object.
-function cleanArgs (cmd) {
-  const args = {}
+function cleanArgs(cmd) {
+  const args = {};
   cmd.options.forEach(o => {
     const key = camelize(o.long.replace(/^--/, ''))
     // if an option is not present and Command has a method with the same name
@@ -54,6 +75,6 @@ function cleanArgs (cmd) {
     if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
       args[key] = cmd[key]
     }
-  })
+  });
   return args
 }
