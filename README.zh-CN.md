197177e01c189030d3f01719b09de9577572acdb
wangchong <wangchong@didachuxing.com>
2020-09-07 13:07:26 +0800

    vmaker create

---
+++ README.zh-CN.md
@@ -0,0 +1,94 @@
+# vue-maker-plus
+
+forked from @vue/cli-service-global
+
+命令行可以使用 `vue-maker-plus` 或者 `vmaker`
+```bash
+vue-maker-plus --version
+vmaker --version
+```
+`serve` `build` `inspect` `lint` 命令的使用方式参数和vue-cli-service一样  
+支持通过vue.config.js文件修改webpack配置  
+```bash
+vmaker serve
+vmaker build
+vmaker inspect
+vmaker lint
+```   
+可参考`vue cli` [vue-cli-service官方文档](https://cli.vuejs.org/zh/guide/cli-service.html)   
+
+-----
+### `vmaker build`
+`vmaker build` 额外增加两个参数 `--srchash` 和 `--no-srchah`   
+
+1.使用`--srchash` 会计算每个源文件MD5值生成.srchash文件到打包目录   
+再次打包时会对比上一次.srchash文件,如果相同则不进行打包   
+2.使用`--no-srchah` 同样会计算每个源文件MD5值生成.srchash文件到打包目录,   
+但不进行对比上一次.srchash文件,每次都进行打包
+
+-----
+### `vmaker create` 创建新项目
+```bash
+vmaker create some-project-name
+
+Options:
+  -d, --default     skip prompts
+  -f, --force       Overwrite target directory if it exists
+  --merge           Merge target directory if it exists
+  -c, --clone       Use git clone when fetching remote preset
+  --no-clone        Do not use git clone when fetching remote preset
+  --disable-config  Disable user vue.maker.config
+  -h, --help        output usage information
+```
+
+`vmaker create`    
+```
+? Enter the git repo of template source: (https://gitee.com/hm496/vue-project-templates.git)
+```
+1. 从templateSource指定的git仓库拉取template文件    
+templateSource 默认值为 https://gitee.com/hm496/vue-project-templates.git   
+2. 指定branch, 如指定branch为www   
+则设置templateSource为 https://gitee.com/hm496/vue-project-templates.git#www   
+
+templateSource中每个文件夹是一个模板     
+模板使用`ejs`语法     
+```
+? Please select a template: (Use arrow keys)
+> mini
+> full
+```
+
+可以通过 `vue.maker.config.js` 设置模板逻辑   
+`vue.maker.config.js` 可以放在每个模板的目录中(template vue.maker.config)   
+也可以放在当前执行`vmaker create`命令的目录 `process.cwd()` 及当前目录的父级目录中(user vue.maker.config)      
+
+```js
+// mini/vue.maker.config.js
+module.exports = (creator) => {
+  // creator 实例
+  // creator.targetDir // 文件输出的文件夹
+  // creator.doNotCopyFiles // 不需要拷贝的文件
+  // creator.templateData // 模板数据
+  return {
+    cliOptions: {}, // 命令行参数
+    doNotCopyFiles: [], // 不需要拷贝的文件
+    templateSource: '', // 模板文件git地址
+    templateData: {}, // 模板数据, 默认只有projectName
+    prompts: [], // inquirer.prompt(prompts), 数据结果会合并到templateData
+    hooks: {
+      // 生成文件前执行
+      beforeCreate () {
+        // 可以在这里修改doNotCopyFiles 和 templateData
+        // creator.doNotCopyFiles
+        // creator.templateData
+      },
+      // 生成文件后执行
+      finishCreate () {
+        // 可以在这里执行git初始化等命令
+        // creator.targetDir // 文件输出的文件夹
+      },
+    }
+  }
+};
+```
+
--- bin/vue-maker-plus.js
+++ bin/vue-maker-plus.js
@@ -1,129 +1,115 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra');
+const program = require('commander');
+const vueMakerPlus = require('../lib/vue-maker-plus');
+const readPkg = require('read-pkg');
 const path = require('path');
-const chalk = require('chalk');
-const Service = require('@vue/cli-service');
-const { toPlugin, findExisting, generateSrcHash, getSrcHash, setSrcHash, getProjectPlugins, setProjectOptionsPlugin } = require('../lib/util');
 
-const babelPlugin = toPlugin('@vue/cli-plugin-babel');
-const eslintPlugin = toPlugin('@vue/cli-plugin-eslint');
-const globalConfigPlugin = require('../lib/globalConfigPlugin');
+const pkg = readPkg.sync({ cwd: path.join(__dirname, '../') });
+program.version(pkg.version);
 
-const context = process.cwd();
+program
+  .command('create <project-name>')
+  .description('create a new project')
+  .option('-d, --default', 'skip prompts')
+  .option('-f, --force', 'Overwrite target directory if it exists')
+  .option('--merge', 'Merge target directory if it exists')
+  .option('-c, --clone', 'Use git clone when fetching remote preset')
+  .option('--no-clone', 'Do not use git clone when fetching remote preset')
+  .option('--disable-config', 'Disable user vue.maker.config')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd);
 
-function resolveEntry(entry) {
-  entry = entry || findExisting(context, [
-    'src/main.js',
-    'src/index.js',
-    'src/App.vue',
-    'src/app.vue',
-    'main.js',
-    'index.js',
-    'App.vue',
-    'app.vue'
-  ]);
+    if (process.argv.includes('-c') || process.argv.includes('--clone')) {
+      options.clone = true
+    }
+    require('../lib/create/index')(name, options);
+  });
 
-  if (!entry) {
-    console.log(chalk.red(`Failed to locate entry file in ${chalk.yellow(context)}.`));
-    console.log(chalk.red(`Valid entry file should be one of: main.js, index.js, App.vue or app.vue.`));
+program
+  .command('serve [entry]')
+  .description('serve a .js or .vue file in development mode with zero config')
+  .option('-o, --open', 'Open browser')
+  .option('-c, --copy', 'Copy local url to clipboard')
+  .option('-p, --port <port>', 'Port used by the server (default: 8080 or next available port)')
+  .option('--mode <mode>', `specify env mode (default: development)`)
+  .option('--host <host>', `specify host`)
+  .option('--https', `use https`)
+  .option('--public <public network URL>', `specify the public network URL for the HMR client`)
+  .option('--skip-plugins <plugins>', `comma-separated list of plugin names to skip for this run`)
+  .action((entry, cmd) => {
+    vueMakerPlus(entry, cleanArgs(cmd));
+  });
 
-    console.log();
-    process.exit(1);
-  }
+program
+  .command('build [entry]')
+  .description('build a .js or .vue file in production mode with zero config')
+  .option('-t, --target <target>', 'Build target (app | lib | wc | wc-async, default: app)')
+  .option('-n, --name <name>', 'name for lib or web-component mode (default: entry filename)')
+  .option('-d, --dest <dir>', 'output directory (default: dist)')
+  .option('--srchash', 'Generate source files hash and Hash diff')
+  .option('--no-srchash', 'Generate source files hash and Build forced')
+  .option('--mode <mode>', `specify env mode (default: production)`)
+  .option('--modern', `build app targeting modern browsers with auto fallback`)
+  .option('--no-unsafe-inline', `build app without introducing inline scripts`)
+  .option('--inline-vue', 'include the Vue module in the final bundle of library or web component target')
+  .option('--formats', `list of output formats for library builds`)
+  .option('--filename <filename>', `file name for output, only usable for 'lib' target (default: value of --name)`)
+  .option('--no-clean', `do not remove the dist directory before building the project`)
+  .option('--report', `generate report.html to help analyze bundle content`)
+  .option('--report-json', 'generate report.json to help analyze bundle content')
+  .option('--skip-plugins <plugins>', `comma-separated list of plugin names to skip for this run`)
+  .option('--watch', `watch for changes`)
+  .option('--stdin', `close when stdin ends`)
+  .action((entry, cmd) => {
+    const options = cleanArgs(cmd);
 
-  if (!fs.pathExistsSync(path.join(context, entry))) {
-    console.log(chalk.red(`Entry file ${chalk.yellow(entry)} does not exist.`));
+    options.srchash = null;
+    if (process.argv.includes('--srchash')) {
+      options.srchash = true;
+    } else if (process.argv.includes('--no-srchash')) {
+      options.srchash = false;
+    }
+    vueMakerPlus(entry, options);
+  });
 
-    console.log();
-    process.exit(1);
-  }
+program
+  .command('inspect [paths...]')
+  .description('inspect the webpack config in a project')
+  .option('--mode <mode>')
+  .option('--rule <ruleName>', 'inspect a specific module rule')
+  .option('--plugin <pluginName>', 'inspect a specific plugin')
+  .option('--rules', 'list all module rule names')
+  .option('--plugins', 'list all plugin names')
+  .option('-v --verbose', 'Show full function definitions in output')
+  .option('--skip-plugins <plugins>', 'comma-separated list of plugin names to skip for this run')
+  .action((paths, cmd) => {
+    vueMakerPlus(void 0, cleanArgs(cmd));
+  });
 
-  return {
-    context,
-    entry
-  };
+program
+  .arguments('<command>')
+  .action(() => {
+    vueMakerPlus();
+  });
+
+program.parse(process.argv);
+
+function camelize(str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
 }
 
-function createService(context, entry, asLib, args) {
-  const projectPlugins = getProjectPlugins(context);
-
-  return new Service(context, {
-    plugins: [
-      setProjectOptionsPlugin(context, entry, asLib, args),
-      babelPlugin,
-      eslintPlugin,
-      globalConfigPlugin(context, entry, asLib, args)
-    ].concat(projectPlugins)
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs(cmd) {
+  const args = {};
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
   });
+  return args
 }
-
-(async function () {
-  const rawArgv = process.argv.slice(2);
-  const args = require('minimist')(rawArgv, {
-    boolean: [
-      // build
-      'modern',
-      'report',
-      'report-json',
-      'watch',
-      // serve
-      'open',
-      'copy',
-      'https',
-      // inspect
-      'verbose'
-    ]
-  });
-  const command = args._[0];
-  const blackOptions = [ 'modern' ];
-
-  blackOptions.forEach(option => {
-    if (args[option]) {
-      args[option] = false;
-      console.log(chalk.red(`Option --${option} is not available.`));
-    }
-  });
-
-  const { context, entry } = resolveEntry();
-  const asLib = args.target && args.target !== 'app';
-  if (asLib) {
-    args.entry = entry;
-  }
-  const service = createService(context, entry, asLib, args);
-
-  // mode
-  const mode = args.mode || (command === 'build' && args.watch ? 'development' : service.modes[command]);
-
-  // srchash
-  if (command === 'build' && args.srchash !== undefined) {
-    service.init(mode);
-    const webpackConfig = service.resolveWebpackConfig();
-    if (webpackConfig.output && webpackConfig.output.path) {
-      if (webpackConfig.output.path === context) {
-        throw new Error(
-          `\n\nConfiguration Error: ` +
-          `Do not set output directory to project root.\n`
-        );
-      }
-      args.clean = false;
-      const newHash = await generateSrcHash(context, [ webpackConfig.output.path + '/**' ]);
-      let oldHash = '';
-      if (fs.pathExistsSync(path.join(webpackConfig.output.path, service.projectOptions.indexPath))) {
-        oldHash = getSrcHash(webpackConfig.output.path);
-      }
-      if (oldHash === newHash && args.srchash) {
-        console.log(chalk.red(`Compiled files are already up-to-date`));
-
-        console.log();
-        process.exit(0);
-        return;
-      } else {
-        fs.emptyDirSync(webpackConfig.output.path);
-        setSrcHash(webpackConfig.output.path, newHash);
-      }
-    }
-  }
-
-  service.run(command, args, rawArgv);
-})();
